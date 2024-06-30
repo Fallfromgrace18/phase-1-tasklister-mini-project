452934ad87e73f23bed886fd1fa4209331c3893a
Fallfromgrace18 <149462981+Fallfromgrace18@users.noreply.github.com>
2024-06-30 09:31:31 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.querySelector('form')
+document.addEventListener('submit', (e)=>{
+
+  e.preventDefault()
+  let task=e.target[0].value
+  let li= document.createElement('li')
+
+  let btn=document.createElement('button')
+  btn.textContent='x'
+  btn.addEventListener('click', (event)=>{
+  console.log(event.target.parentNode)
+  event.target.parentNode.remove()
+  })
+  li.textContent=  `${task} `
+  li.appendChild(btn)
+  console.log(li)
+  document.getElementById('tasks').append(li)
+})
+})
+
+
+  
